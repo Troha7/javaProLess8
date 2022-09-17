ad3850e5e2564aa6a62cbc1fb07d96b85b336e91
Dmytro Trotsenko <trohae7@gmail.com>
2022-09-17 20:36:18 +0300

    Fixed trow ArraySizeException

--- src/ua/hillelit/lms/homeWork/ArrayValueCalculator.java
+++ src/ua/hillelit/lms/homeWork/ArrayValueCalculator.java
@@ -7,33 +7,31 @@ public class ArrayValueCalculator {
 
   public static void main(String[] args) {
 
-    String[][] strArr = { {"1", "1", "1", "1"},
-                          {"2", "2", "2", "2"},
-                          {"3", "3", "3", "3"},
-                          {"4", "4", "4", "4"} };
+    String[][] strArr = {{"1", "2", "3", "4"}, {"2"}, {"3", "5", "8", "9", "777", "888"}, null};
 
     try {
       int sumArr = doCalc(strArr);
       System.out.println("Sum Integer in array = " + sumArr);
     } catch (ArraySizeException | ArrayDataException e) {
       System.out.println(e.getMessage());
-    } catch (NullPointerException e) {
-      System.out.println("Input array: [null]");
     }
 
   }
 
   public static int doCalc(String[][] strArr) throws ArraySizeException {
 
-    if (strArr[0].length != 4 || strArr.length != 4) {
+    if (strArr == null || strArr.length != 4) {
       throw new ArraySizeException(4, 4);
     }
 
     int sum = 0;
-    int len = strArr.length;
 
-    for (int i = 0; i < len; i++) {
-      for (int j = 0; j < len; j++) {
+    for (int i = 0; i < strArr.length; i++) {
+      for (int j = 0; j < strArr[i].length; j++) {
+
+        if (strArr[i].length != 4) {
+          throw new ArraySizeException(4, 4);
+        }
 
         try {
           sum += Integer.parseInt(strArr[i][j]);
